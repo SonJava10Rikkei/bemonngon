fa53c10e625c26775286886c83011a8e215fe018
SonJava10Rikkei <sonjapan7@gmail.com>
2023-12-15 17:04:05 +0700

    deploy 1

---
+++ src/main/java/com/gutrend/bemonngon/BemonngonApplication.java
@@ -0,0 +1,13 @@
+package com.gutrend.bemonngon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemonngonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemonngonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gutrend/bemonngon/BemonngonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutrend.bemonngon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemonngonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
